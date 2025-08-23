7570d7887fb5078903415cf02f670069ca4eb913
Kirill <kirillkongrichanichenko@gmail.com>
2025-08-22 17:02:00 -0700

    comments

--- Project/backend/config.js
+++ Project/backend/config.js
@@ -2,12 +2,18 @@ const mysql = require(`mysql2`);
 const dotenv = require(`dotenv`);
 dotenv.config();
 
+// This application uses an .env file to manage multiple connections to mysql workbench.
+// To run this on local machine either:
+// 1. create a .env file with information pertaining to your local machine 
+// OR
+// 2. change the user and password lines to your pertaining local machine
+
 // create connection to airport_system_db
 const connection = mysql.createConnection
 ({
   host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER, // Change to local machine
-  password: process.env.MYSQL_PASSWORD, // Change to local machine
+  user: process.env.MYSQL_USER, // Change to local machine username
+  password: process.env.MYSQL_PASSWORD, // Change to local machine password
   database: process.env.MYSQL_DATABASE
 });
 
